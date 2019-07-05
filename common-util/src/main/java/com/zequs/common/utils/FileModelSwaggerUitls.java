f06d92ff8d521a0ab3687eae7c5aa1a7613fd1b2
zequs <563248403@qq.com>
2019-07-05 16:51:30 +0800

    zequs

--- common-util/src/main/java/com/zequs/common/utils/FileModelSwaggerUitls.java
+++
@@ -1,8 +0,0 @@
-package com.zequs.common.util;
-
-/**
- * @author zequs
- * @version $Id: common-util, v0.1 2019 06 29 Exp $
- */
-public class FileModelSwaggerUitls {
-}
--- swagger-demo/src/main/java/com/mao/swagger/beans/Person.java
+++
@@ -1,8 +0,0 @@
-package com.mao.swagger.beans;
-
-/**
- * @author zequs
- * @version $Id: swagger-demo, v0.1 2019 06 28 Exp $
- */
-public class Person {
-}

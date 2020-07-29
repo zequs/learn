9737f728e5d761a34ab89cae32755558b0fc5350
zequs <563248403@qq.com>
2020-07-29 21:44:30 +0800

    forkjointask注解

--- java-se/se-demo/src/main/java/com/zequs/demo/se/lambda/test/StreamTest.java
+++ java-se/se-demo/src/main/java/com/zequs/demo/se/lambda/test/StreamTest.java
@@ -1,11 +1,13 @@
 package com.zequs.demo.se.lambda.test;
 
+import com.zequs.demo.se.Test.User;
 import com.zequs.demo.se.lambda.Apple;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StreamTest {
@@ -17,7 +19,15 @@ public class StreamTest {
 //        filterTest(list);
 //        mapTest(list);
 //        sortedTest(list);
-        forEachTest();
+//        forEachTest();
+//        minTest(list);
+//        maxTest(list);
+//        anyMatchTest(list);
+//        allMatchTest(list);
+//        noneMatchTest(list);
+//        findFirstTest(list);
+//        findFirstTest(new ArrayList<>());
+        findAnyTest(list);
     }
 
     /**
@@ -97,7 +107,6 @@ public class StreamTest {
 
     /**
      * forEach与forEachOrdered的区别就是在并发处理时forEachOrdered还是会按照原来顺序进行处理。forEach随机顺序处理
-     *
      */
     public static void forEachTest() {
         List<String> list = new ArrayList<>();
@@ -136,15 +145,143 @@ public class StreamTest {
     /**
      * reduce有3个重载方法
      * 一个参数
-     * Optional<T> reduce(BinaryOperator<T> accumulator)
-     * 两个参数
+     * Optional<T> reduce(BinaryOperator<T> accumulator) {@link java.util.function.BiFunction#apply(Object, Object)}
+     * 两个参数identity是初始值
      * T reduce(T identity, BinaryOperator<T> accumulator);
-     * 三个参数
+     * 三个参数,combiner并行的时候有用
      * <U> U reduce(U identity,
-     *                  BiFunction<U, ? super T, U> accumulator,
-     *                  BinaryOperator<U> combiner);
+     * BiFunction<U, ? super T, U> accumulator,
+     * BinaryOperator<U> combiner);
      */
-    public static void reduce() {
+    public static void reduceTest(List<User> list) {
+        //返回年龄最大的用户
+        Optional<User> optional = list.stream().reduce(((user, user2) -> {
+            return user.getAge() > user2.getAge() ? user : user2;
+        }));
+
+        User user = optional.get();
 
     }
+
+    /**
+     * 流中最小的数字
+     * 接收参数Comparator<T,R>参数，格式 T1,T2 -> int
+     * t1 > t2 时返回1，t1 < t2时返回-1（注意：跟max一样的，都是大于返回1）
+     *
+     * @param list
+     */
+    public static void minTest(List<Apple> list) {
+        //返回年龄最大的用户
+        Optional<Apple> optional = list.stream().min((a1, a2) -> {
+            return a1.getWeight() >= a2.getWeight() ? 1 : -1;
+        });
+
+        Apple apple = optional.get();
+        System.out.println(apple.toString());
+
+    }
+
+    /**
+     * 流中最大的数
+     * 接收参数Comparator<T,R>参数，格式 T1,T2 -> int
+     * t1 > t2 时返回1，t1 < t2时返回-1（注意：跟min一样的，都是大于返回1）
+     *
+     * @param list
+     */
+    public static void maxTest(List<Apple> list) {
+        //返回年龄最大的用户
+        Optional<Apple> optional = list.stream().max((a1, a2) -> {
+            return a1.getWeight() >= a2.getWeight() ? 1 : -1;
+        });
+        Apple apple = optional.get();
+        System.out.println(apple.toString());
+    }
+
+    /**
+     * 流中元素个数
+     *
+     * @param list
+     */
+    public static void countTest(List<Apple> list) {
+        //返回年龄最大的用户
+        long count = list.stream().count();
+        System.out.println(count);
+    }
+
+    /**
+     * 流中是否有一个元素匹配
+     * 接收参数Predicate 格式：T -> boolean
+     *
+     * @param list
+     */
+    public static void anyMatchTest(List<Apple> list) {
+        //返回年龄最大的用户
+        boolean result = list.stream().anyMatch(apple -> {
+            return apple.getColor().equals("red");
+        });
+        System.out.println(result);
+    }
+
+    /**
+     * 流中元素是否全部符合
+     * 接收参数Predicate 格式：T -> boolean
+     *
+     * @param list
+     */
+    public static void allMatchTest(List<Apple> list) {
+        //返回年龄最大的用户
+        boolean result = list.stream().allMatch(apple -> {
+            return apple.getWeight() > 1;
+        });
+        System.out.println(result);
+    }
+
+    /**
+     * 流中元素全部不符合为ture
+     * 接收参数Predicate 格式：T -> boolean
+     *
+     * @param list
+     */
+    public static void noneMatchTest(List<Apple> list) {
+        //返回年龄最大的用户
+        boolean result = list.stream().noneMatch(apple -> {
+            return apple.getWeight() > 21;
+        });
+        System.out.println(result);
+    }
+
+    /**
+     * 流中元素第一个元素
+     * 接收参数Predicate 格式：T -> boolean
+     *
+     * @param list
+     */
+    public static void findFirstTest(List<Apple> list) {
+        //返回年龄最大的用户
+        Optional<Apple> apple = list.stream().findFirst();
+        if (apple.isPresent()) {
+            System.out.println(apple.get());
+        } else {
+            System.out.println("空");
+        }
+    }
+
+    /**
+     * 流中元素任意一个元素
+     * stream()一般返回第一个，
+     * parallelStream()流返回任意一个
+     * 接收参数Predicate 格式：T -> boolean
+     *
+     * @param list
+     */
+    public static void findAnyTest(List<Apple> list) {
+        //返回年龄最大的用户
+        Optional<Apple> apple = list.parallelStream().findAny();
+        if (apple.isPresent()) {
+            System.out.println(apple.get());
+        } else {
+            System.out.println("空");
+        }
+    }
+
 }
